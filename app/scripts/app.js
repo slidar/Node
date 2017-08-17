016f9b80b05addf680e4592bb63c081255753306
slidar <slidarv@yahoo.com>
2017-08-17 15:03:24 -0700

    Assignment 3 completed

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,1 +1,14 @@
 angular.module('blocJams', []);
+
+$stateProvider {
+    .state('collection', {
+        url: "/collection",
+        templateUrl: 'collection.html'
+    })
+  }
+
+$stateProvider.state('collection', {
+  templateUrl: function ($stateParams){
+    return '/partials/collection.' + $stateParams.filterBy + '.html';
+  }
+})
