313511465b9485537e7e75fc9f3f8c07db7b9fe7
slidar <slidarv@yahoo.com>
2017-09-21 19:20:20 -0700

    checkpoint 10 complete

--- app/scripts/services/SongPlayer.js
+++ app/scripts/services/SongPlayer.js
@@ -1,5 +1,5 @@
 (function() {
-    function SongPlayer(Fixtures) {
+    function SongPlayer($rootScope, Fixtures) {
          var SongPlayer = {};
 
          var currentAlbum = Fixtures.getAlbum();
@@ -20,6 +20,12 @@
         */
         SongPlayer.currentSong = null;
 
+        /**
+        * @desc Current playback time (in seconds) of currently playing song
+        * @type {Number}
+        */
+        SongPlayer.currentTime = null;
+
          var currentBuzzObject = null;
 
          /**
@@ -37,6 +43,12 @@
             currentBuzzObject = new buzz.sound(song.audioUrl, {
                 formats: ['mp3'],
                 preload: true
+            });
+
+            currentBuzzObject.bind('timeupdate', function() {
+                $rootScope.$apply(function() {
+                    SongPlayer.currentTime = currentBuzzObject.getTime();
+                });
             });
 
            SongPlayer.currentSong = song;
@@ -112,6 +124,17 @@
               song.playing = null;
             };
 
+            /**
+            * @function setCurrentTime
+            * @desc Set current time (in seconds) of currently playing song
+            * @param {Number} time
+            */
+            SongPlayer.setCurrentTime = function(time) {
+                if (currentBuzzObject) {
+                    currentBuzzObject.setTime(time);
+                }
+            };
+
             return SongPlayer;
 
 
@@ -122,5 +145,5 @@
 
     angular
         .module('blocJams')
-        .factory('SongPlayer', ['Fixtures', SongPlayer]);
+        .factory('SongPlayer', ['$rootScope', 'Fixtures', SongPlayer]);
 })();
